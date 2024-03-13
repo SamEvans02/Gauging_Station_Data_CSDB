4454ca968f9eb9208cf9468f1238726e652616fa
blue-Cloud32 <cau45glose@gmail.com>
2024-03-13 17:06:56 -0600

    added new file

--- get_day_avg.py
+++ get_day_avg.py
@@ -10,11 +10,39 @@ if __name__ == "__main__":
     host = os.getenv('POSTGRES_HOST'),
     port = 5432
   )
+
+  date = '2024-2-29' # 2024-2-13 had NONE values
   cur = conn.cursor()
-  cur.execute("SELECT * FROM river_data")
-  rows = cur.fetchall()
-  for row in rows:
-    print(row)
+  cur.execute(f"SELECT gage_val, discharge_val, disolved_val, ph_val, temp_val FROM river_data WHERE val_date = \'{date}\'")
+  
+  date_values = cur.fetchall()
+  total_time_events = len(date_values)
+
+  gage_sum = 0
+  discharge_sum = 0
+  disolved_sum = 0
+  ph_sum = 0
+  temp_sum = 0
+
+  for time_event in date_values:
+    # print(f"{time_event[0]} {time_event[1]} {time_event[2]} {time_event[3]} {time_event[4]}")
+    
+    gage_sum += time_event[0]
+    discharge_sum += time_event[1]
+    disolved_sum += time_event[2]
+    ph_sum += time_event[3]
+    temp_sum += time_event[4]
+
+  print(f"{gage_sum / total_time_events} {discharge_sum / total_time_events} \
+        {disolved_sum / total_time_events} {ph_sum / total_time_events} {temp_sum / total_time_events}")
+
+  # cur.execute(f"SELECT * FROM river_data WHERE gage_val = NULL OR discharge_val = NULL OR disolved_val = NULL OR ph_val = NULL OR temp_val = NULL")
+  # rows = cur.fetchall()
+  # for row in rows:
+  #   print(row)
+
+
+
 
   cur.close()
   conn.close()
