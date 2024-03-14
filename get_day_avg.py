2ac584b49b86d57eefe005c985640d78df56bef4
blue-Cloud32 <cau45glose@gmail.com>
2024-03-13 20:43:54 -0600

    modified to make code more funtional

--- get_day_avg.py
+++ get_day_avg.py
@@ -2,7 +2,7 @@ import requests
 import psycopg2
 import os
 
-if __name__ == "__main__":
+def get_avg(date):
   conn = psycopg2.connect(
     dbname = os.getenv('POSTGRES_DB'),
     user = os.getenv('POSTGRES_USER'),
@@ -10,8 +10,6 @@ if __name__ == "__main__":
     host = os.getenv('POSTGRES_HOST'),
     port = 5432
   )
-
-  date = '2024-2-29' # 2024-2-13 had NONE values
   cur = conn.cursor()
   cur.execute(f"SELECT gage_val, discharge_val, disolved_val, ph_val, temp_val FROM river_data WHERE val_date = \'{date}\'")
   
@@ -33,16 +31,18 @@ if __name__ == "__main__":
     ph_sum += time_event[3]
     temp_sum += time_event[4]
 
-  print(f"{gage_sum / total_time_events} {discharge_sum / total_time_events} \
-        {disolved_sum / total_time_events} {ph_sum / total_time_events} {temp_sum / total_time_events}")
-
-  # cur.execute(f"SELECT * FROM river_data WHERE gage_val = NULL OR discharge_val = NULL OR disolved_val = NULL OR ph_val = NULL OR temp_val = NULL")
-  # rows = cur.fetchall()
-  # for row in rows:
-  #   print(row)
-
-
-
-
   cur.close()
   conn.close()
+
+  gage_avg = gage_sum / total_time_events
+  discharge_avg = discharge_sum / total_time_events
+  disolved_avg = disolved_sum / total_time_events
+  ph_avg = ph_sum / total_time_events
+  temp_avg = temp_sum / total_time_events
+  
+  print(f"date: {date}")
+  print(f"{round(gage_avg, 2)} {round(discharge_avg, 2)} {round(disolved_avg, 2)} {round(ph_avg, 2)} {round(temp_avg, 2)}")
+
+if __name__ == "__main__":
+  date = '2024-3-2' # 2024-2-13 had NONE values
+  get_avg(date)
